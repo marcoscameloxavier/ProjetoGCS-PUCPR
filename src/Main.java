a54a1091522b56e7c135a7d20b8340322a4abfbe
NelsonAmor <150303761+NelsonAmor@users.noreply.github.com>
2023-11-29 20:48:02 -0300

    Erros do Nelson

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Projeto GCS!");
+
+        System.out.println("Commit pelo IntelliJ com push");
+
     }
 }
