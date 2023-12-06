aa689e752093ff6546afe5f94a476de94f71f057
unknown <chico.ricardo@uol.com.br>
2023-12-06 20:51:29 -0300

    pagina-de-login

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import com.intellij.codeInsight.template.postfix.templates.SoutPostfixTemplate;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Projeto GCS!");
+        System.out.println("Login: Chico");
+        System.out.println("Senha: *****");
     }
 }
