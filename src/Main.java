9cf5122686522ed4e043e243c5e7eb032ad4839c
NelsonAmor <150303761+NelsonAmor@users.noreply.github.com>
2023-12-06 21:22:26 -0300

    Carrinho de compras

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Projeto GCS!");
+
+        System.out.println("Carrinho de compras");
+        System.out.println("Produto 1 - Preco R$ 100,00 - Qtd  - 2");
+        System.out.println("Produto 2 - Preco R$ 50,00  - Qtd  - 1");
+
     }
 }
