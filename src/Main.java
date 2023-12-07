cb473bb87a13dfec31db65e29321c430d120e68d
unknown <chico.ricardo@uol.com.br>
2023-12-06 21:43:23 -0300

    Vitrine de Produtos

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Projeto GCS!");
+        System.out.println("Vitrine de Produtos");
+        System.out.println("1- Ecobag STJ");
+        System.out.println("2- Calendário STJ");
+        System.out.println("3- Agenda STJ");
     }
 }
