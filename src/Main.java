50d5236f075260b729a464269f41d552883a3ade
NelsonAmor <150303761+NelsonAmor@users.noreply.github.com>
2023-12-06 21:31:31 -0300

    Cadastro de produtos

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,23 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Projeto GCS!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println("Cadastro de produto");
+        System.out.println("Nome do produto : ecobag STJ");
+        System.out.println("Preço : 50,00");
+        System.out.println("Qtd: 100");
     }
 }
